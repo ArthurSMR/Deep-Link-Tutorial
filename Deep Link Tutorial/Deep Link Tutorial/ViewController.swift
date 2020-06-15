0bce95590682a6f1a2b557351337541229af7fd7
Arthur Rodrigues <arthursmr17@gmail.com>
2020-06-15 10:19:15 -0300

    project init

---
+++ Deep Link Tutorial/Deep Link Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deep Link Tutorial
+//
+//  Created by Arthur Rodrigues on 15/06/20.
+//  Copyright © 2020 Arthur Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
